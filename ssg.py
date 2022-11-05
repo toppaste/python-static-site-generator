3cb4bcd5e129827f746784eaa098efed2e03ab8b
toppaste <stefano.toppan@electrolux.com>
2022-11-05 17:28:39 +0100

    The Parser Class

--- ssg.py
+++ ssg.py
@@ -7,8 +7,8 @@ import ssg.parsers
 def main (source="content", dest="dist"):
     config = {
         "source": source,
-        "dest": dest
-        "parsers": [ssg.parsers.ResourceParser(), ]
+        "dest": dest,
+        "parsers": [ssg.parsers.ResourceParser()]
         }
 
     Site(**config).build()
