1ee4c1bc0a73b90a1127a9fce3066cf1360807e4
toppaste <stefano.toppan@electrolux.com>
2022-11-07 12:12:10 +0100

    Parser Subclasses

--- ssg/site.py
+++ ssg/site.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 
 class Site:
 
@@ -21,7 +22,7 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print("Not Implemented")
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
@@ -30,3 +31,7 @@ class Site:
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
